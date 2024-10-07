3e0f198c4e5fdd80e635413d33f742ed549dddc3
MarcoApunto <ferreiras.marcoa@gmail.com>
2024-10-07 17:48:31 +0200

    cambios importantes en renombre de carpetas y subidas de ejercicios

---
+++ JavaScript/ejemplosTeoría/script.js
@@ -0,0 +1,12 @@
+let frutas = ["Manzana", "Banana"];
+console.log(frutas); // Manzana y Banana
+console.log(frutas.length); // 2
+let primero = frutas[0]; // Manzana
+let ultimo = frutas[frutas.length - 1]; // Banana
+let nuevaLongitud = frutas.push("Naranja"); // Añade "Naranja" al final
+// ["Manzana", "Banana", "Naranja"]
+ultimo = frutas.pop(); // Elimina "Naranja" del final
+// ["Manzana", "Banana"]
+var frase = "Hola Mundo!";
+alert(frase);
+
