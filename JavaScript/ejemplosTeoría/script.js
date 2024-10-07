a54fb74a1956057666de424bab8a9947dd413ef5
MarcoApunto <ferreiras.marcoa@gmail.com>
2024-10-07 18:53:34 +0200

    ejercicios

--- JavaScript/ejemplosTeoría/script.js
+++ JavaScript/ejemplosTeoría/script.js
@@ -1,3 +1,4 @@
+/*
 let frutas = ["Manzana", "Banana"];
 console.log(frutas); // Manzana y Banana
 console.log(frutas.length); // 2
@@ -9,4 +10,25 @@ ultimo = frutas.pop(); // Elimina "Naranja" del final
 // ["Manzana", "Banana"]
 var frase = "Hola Mundo!";
 alert(frase);
+*/
+
+/*
+var foo = 0;
+switch (foo) {
+	case -1:
+		console.log("1 negativo");
+		break;
+	case 0: // foo es 0, por lo tanto se cumple la condición y se ejecutara el siguiente bloque
+		console.log(0);
+	// NOTA: el "break" olvidado debería estar aquí
+	case 1: // No hay sentencia "break" en el 'case 0:', por lo tanto este caso también será ejecutado
+		console.log(1);
+		break; // Al encontrar un "break", no será ejecutado el 'case 2:'
+	case 2:
+		console.log(2);
+		break;
+	default:
+		console.log("default");
+}
+*/
 
