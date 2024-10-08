5ce6ad9f623fb1296873727412486f822be98dc0
MarcoApunto <ferreiras.marcoa@gmail.com>
2024-10-08 16:58:10 +0200

    ejercicios

--- JavaScript/ejercicios-clase4/ej_4-6/script.js
+++ JavaScript/ejercicios-clase4/ej_4-6/script.js
@@ -4,14 +4,13 @@ const arrComp = [...arr];
 arrComp.sort;
 
 let aux = "";
-for (let i = 0; i < arr.length - 1; i++) {
+for (let i = 0; i <= arr.length - 1; i++) {
 	for (let j = 0; j < arr.length - 1; j++) {
 		if (arr[j] > arr[j+1]) {
 			aux = arr[j];
 			arr[j] = arr[j+1];
 			arr[j+1] = aux;
 		}
-		
 	}
 }
 
