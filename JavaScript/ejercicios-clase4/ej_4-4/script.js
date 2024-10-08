7de7ec0a263ae09f9b7929647a618130f0833365
MarcoApunto <ferreiras.marcoa@gmail.com>
2024-10-08 20:27:35 +0200

    ejercicios

--- JavaScript/ejercicios-clase4/ej_4-4/script.js
+++ JavaScript/ejercicios-clase4/ej_4-4/script.js
@@ -10,14 +10,13 @@ do {
 	pass = prompt("Password: \n(Pista: Ferreira)");
 	if (username == user && password == pass) {
 		alert("Correcto!");
-		break;
 	} else if (username != user) {
 		alert("Username incorrecto");
 	} else {
 		alert("Password incorrecto");
 	}
 	if (count == 0) {
-		alert("El sistema ha sido bloqueado por haber realizado 3 intentos fallidos")
+		alert("El sistema ha sido bloqueado por haber realizado 3 intentos fallidos");
 		break;
 	}
 	alert("Quedan " + count + " intentos");
---
+++ JavaScript/ejercicios-clase5/script.js
@@ -0,0 +1,45 @@
+function countNumbers (cn) {
+
+	let count = 0;
+	while (cn != 0) {
+		cn = Math.floor(cn / 10);
+		count++;
+	}
+
+	return count;
+}
+
+console.log(countNumbers(12345));
+
+function undln (n) {
+
+	let ret = "";
+	while (n != 0) {
+		ret += "_ "
+		n -= 1;
+	}
+	return ret;
+}
+
+console.log(undln(7));
+
+function sequence() {
+
+	let seq = "";
+	for (let i = 0; i < 12; i++){
+		if (i % 3 == 0) {
+			seq += "*";
+		} else if (i % 3 == 1) {
+			seq += "+";
+		} else {
+			seq += "_";
+		}
+	}
+	return seq;
+}
+
+console.log(sequence());
+
+(tree) => tree = "*\n**\n***\n****\n*****";
+
+console.log(tree());
