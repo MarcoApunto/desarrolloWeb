34bee98d67125c57619e6b3202f7adef4163123d
MarcoApunto <ferreiras.marcoa@gmail.com>
2024-10-09 19:02:47 +0200

    ejercicios

--- JavaScript/ejercicios-clase5/script.js
+++ JavaScript/ejercicios-clase5/script.js
@@ -3,7 +3,7 @@ function countNumbers() {
 	let cn = prompt("Introduzca un número a contar: ");
 	let count = 0;
 	while (cn != 0) {
-		cn = Math.floor(cn / 10);
+		cn = (cn - (cn % 10 )) / 10;
 		count++;
 	}
 	alert("El número tiene " + count + " dígitos.");
@@ -38,3 +38,44 @@ function sequence() {
 function tree() {
 	alert("*\n**\n***\n****\n*****");
 }
+
+function monthCount (monthComplete) {
+	return month[monthComplete - 1];
+}
+
+function diffDays (dateOne, dateTwo) {
+
+	let [dayOne, montOne] = dateOne;
+	let [dayTwo, montTwo] = dateTwo;
+}
+
+/*
+function diffDays (dateOne, dateTwo) {
+
+	const month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+	let [dayOne, montOne] = dateOne;
+	let [dayTwo, montTwo] = dateTwo;
+
+	let daysMontOne = 0;
+	let daysMontTwo = 0;
+	for (let i = 0; i < (month[montOne - 1] - dayOne); i++) {
+		daysMontOne++;
+	}
+	for (let j = 0; j < (month[montTwo - 1] + (dayTwo - month[montTwo - 1])); j++) {
+		daysMontTwo++;
+	}
+
+	let daysTotal = daysMontOne + daysMontTwo;
+
+	//console.log(mont[montOne - 1] - dayOne);
+	//console.log(mont[montTwo - 1] + (dayTwo - mont[montTwo - 1]));
+	
+	return daysTotal;
+}
+
+console.log(diffDays([20, 7], [30, 8]));
+
+// De mes en mes, hecho
+// Por hacer, saltos de meses: 2 a mas
+*/
