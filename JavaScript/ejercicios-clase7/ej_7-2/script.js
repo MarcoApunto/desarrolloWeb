0ddf5886c56035568d179b8bbfc607688d9ec59b
MarcoApunto <ferreiras.marcoa@gmail.com>
2024-10-11 18:10:32 +0200

    ejercicios clase 7-2 - WIP

---
+++ JavaScript/ejercicios-clase7/ej_7-2/script.js
@@ -0,0 +1,11 @@
+/*
+Completar la función muestra de JavaScript para que realice lo siguiente:
+
+    Ocultar el enlace Seguir leyendo.
+    Mostrar el texto incluido dentro del span
+
+*/
+
+function muestra() {
+	
+}
