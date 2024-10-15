02d77a5854e67da873a659d304620b4ef4a1b1bb
MarcoApunto <ferreiras.marcoa@gmail.com>
2024-10-15 19:32:33 +0200

    ejercicios

--- createTD.js
+++
@@ -1,8 +0,0 @@
-function createTD(texto){
-	let nodoText = document.createTextNode(texto);
-	let nodotd = document.createElement("td");
-	nodotd.appendChild(nodoText);
-	return nodotd;
-}
-
-document.body.appendChild(createTD("Hola"));
