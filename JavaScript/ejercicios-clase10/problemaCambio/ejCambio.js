8310182d8534644b0a98d59eb6dae1b2b320be81
MarcoApunto <ferreiras.marcoa@gmail.com>
2024-10-22 08:18:29 +0200

    arreglado copia array

--- JavaScript/ejercicios-clase10/problemaCambio/ejCambio.js
+++ JavaScript/ejercicios-clase10/problemaCambio/ejCambio.js
@@ -1,12 +1,12 @@
 // 234,27€
 
 let caja = [[500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01],
-			[  1,   0,   0,  0,  2,  0, 1, 1, 1,   0,     0,   0,    0,    0,    0]];
+			[  1,   0,   0,  0,  2,  1, 5, 5, 10,  2,   5,   5,    0,    3,    0]];
 
 let pago = [[500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01],
 			[  0,   0,   0,  1,  0,  0, 0, 0, 0,   0,   0,   0,    0,    0,    0]];
 
-let importe = 40;
+let importe = 27.46;
 
 
 function total(efectivo) {
@@ -44,30 +44,32 @@ if (importe - pago[0][pago[0].length - 1] == 0) { //Importe justo
 	} else {
 		//Si el cliente ha dado de menos
 		if (pago[0][pago[0].length - 1] < importe) {
-			alert("Señor! Falta dinero.")
+			console.log("Señor! Falta dinero.")
 		} else {
 			let cambio = pago[0][pago[0].length - 1] - importe;
 			console.log("A devolver: " + cambio);
 
 			let retBuscar = buscar(caja, cambio);
-			let auxArr = caja.map((x) => x);
-			console.log(caja[1]);
-			console.log(auxArr[1]);
+			// map() aplica slice() a cada elemento de caja, el array original.
+			// Por así decirlo, transforma los datos para que no tengan relación
+			// y poder manipular la copia sin tener cambios en el original.
+			let auxArr = caja.map(auxArr => auxArr.slice());
 
 			for (let i = retBuscar; i < caja[1].length; i++) {
-				console.log(caja[0][i]);
-				while (caja[1][i] > 0) {
-					cambio -= auxArr[0][i];
-					console.log("En la caja " + auxArr[1][i]);
+				while (auxArr[1][i] > 0 && cambio >= auxArr[0][i]) {
+					console.log("En la caja hay " + auxArr[1][i] + " billete de " + auxArr[0][i] + "€");
+					cambio = parseFloat((cambio - auxArr[0][i]).toFixed(2));
 					auxArr[1][i] -= 1;
+					console.log("Usado " + auxArr[1][i] + " billete de " + auxArr[0][i] + "€");
 				}
 			}
-			if (cambio == 0){
-					console.log("Cambio realizado")}
-			else {
-				console.log("No ha habido efectivo")
+			if (cambio === 0) {
+				console.log("Cambio realizado");
+			} else {
+				console.log("No ha habido suficiente efectivo para completar el cambio");
 			}
-			console.log(caja[1]);
+			console.log("Array copia, a: " + auxArr[1]);
+			console.log("Array original: " + caja[1]);
 		}
 	}
 }
