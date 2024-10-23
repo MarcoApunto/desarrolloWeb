ef58eeafbf7109dd6d746182837325f3ac56ab9e
MarcoApunto <ferreiras.marcoa@gmail.com>
2024-10-23 23:42:16 +0200

    chauchat

--- JavaScript/ejercicios-extra/chauchat/chauchat.js
+++ JavaScript/ejercicios-extra/chauchat/chauchat.js
@@ -1,5 +1,6 @@
 function chauchat(cargador) {
 
+	// 80% que el arma se atasque. Si ocurre eso, no habrá tiros.
 	if (Math.random() * 100 <= 80) {
 		console.log("Illo, me he quedao pillá!");
 		return ;
