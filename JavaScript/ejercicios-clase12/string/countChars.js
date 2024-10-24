b6bf6f674fc7b3fbd0c3e7d376ad3150da564ad9
Marco Antonio Ferreira Santos <91089291+MarcoApunto@users.noreply.github.com>
2024-10-24 23:37:56 +0200

    ejercicio countChars

--- JavaScript/ejercicios-clase12/string/countChars.js
+++ JavaScript/ejercicios-clase12/string/countChars.js
@@ -4,14 +4,13 @@ function countChars() {
 	let aux = 0;
 
 	for (let i = 0; i < strSplited.length; i++) {
-		if (strSplited[i].length == 2){
-		}
-		else {
+		if (strSplited[i].length !== 2){
 			for (let j = 0; j < strSplited[i].length; j++) {
 				if (strSplited[i][j] == 'a')
 					aux++;
 			}
 		}
+		
 	}
 	console.log(strSplited);
 	alert("En las palabras impares he contado " + aux + " letras a.")
