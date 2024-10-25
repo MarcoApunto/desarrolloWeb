d834209b905cda79e995b5aaadb3c4f4ceb9b17d
MarcoApunto <ferreiras.marcoa@gmail.com>
2024-10-25 03:06:28 +0200

    ejercicio_1 array

--- JavaScript/ejercicios-clase12/array/script.js
+++ JavaScript/ejercicios-clase12/array/script.js
@@ -1,17 +1,25 @@
 /*
-[prompt("Introduzca el 1º: "), prompt("Introduzca el 2º: "), prompt("Introduzca el 3º: ")],
-							  [prompt("Introduzca el 4º: "), prompt("Introduzca el 5º: "), prompt("Introduzca el 6º: ")],
-							  [prompt("Introduzca el 7º: "), prompt("Introduzca el 8º: "), prompt("Introduzca el 9º: ")]
+DATO ESTABLECIDO:
 
+["aslkdja", 4, 6], ["asofja", "gfoijoi", 64], [353, 64, "alksfjhaf"]
 */
 
 function showRowEvenColOdd() {
 
-	// alert("Introduzca 9 datos, sean númericos o palabras, a continuación.")
-	const arrUser = new Array(["aslkdja", 4, 6], ["asofja", "gfoijoi", 64], [353, 64, "alksfjhaf"]);
+	alert("Introduzca a continuación 9 datos, sean númericos o palabras.")
+	const arrUser = new Array(
+		[prompt("Introduzca el 1º: "), prompt("Introduzca el 2º: "), prompt("Introduzca el 3º: ")],
+		[prompt("Introduzca el 4º: "), prompt("Introduzca el 5º: "), prompt("Introduzca el 6º: ")],
+		[prompt("Introduzca el 7º: "), prompt("Introduzca el 8º: "), prompt("Introduzca el 9º: ")]
+	);
 
-	const mapArr = arrUser.map(e => e.length - 1 % 3 == 0)
-	console.log(mapArr);
+	arrUser.forEach((path, i) => {
+		path.forEach((elem, j) => {
+			if (i % 2 == 0 && j % 2 != 0) {
+				alert(elem);
+			}
+		});
+	});
 }
 
 showRowEvenColOdd();
